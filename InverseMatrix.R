e032b2ffb3d7ace77a3e462b969ddb65b635cdbf
Payombar <moj.payombar@covidien.com>
2016-03-14 21:18:25 -0700

    Added InsertMatrix

---
+++ InverseMatrix.R
@@ -0,0 +1,24 @@
+makeInvMatrix <- function(x = matrix()) {
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cacheInv<- function(x, ...) {
+  inv = x$getinv()
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  x$setinv(inv)
+  return(inv)
+}
+
